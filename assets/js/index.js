5a67216b1161dcf8d1523b4298996e57bce85113
Valentina Cifras Herrera <valentina.cifras@gmail.com>
2024-02-17 23:04:45 -0300

    se agregó smooth scroll para dirigirse a las distintas secciones de la pagina

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,29 @@
+// Smooth Scroll hacia las distintas secciones de la pagina por medio de los links del navbar
+
+$(document).ready(function(){
+    $("a").on('click',function(event){
+        if(this.hash !=="") {
+            event.preventDefault();
+            var hash = this.hash;
+
+
+            $('html,body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+            
+            window.location.hash = hash;
+            });
+         }
+      });
+});
+
+
+
+
+
+// Esta es la alerta que aparece al hacer click en el boton enviar del formulario
+
 $(document).ready(function(){
     $('#enviar').on("click",function(){
         alert("Tú mensaje se envió correctamente");
